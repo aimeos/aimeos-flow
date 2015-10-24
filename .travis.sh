29b790c5530675e3787df85626cc86528246c551
Toco <toco@aimeos.org>
2015-10-24 13:20:01 +0200

    Added travis-ci test files

---
+++ .travis.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+set -ev
+
+cat composer.json | sed 's/^\}$/,\
+    "minimum-stability": "dev",\
+    "prefer-stable": true,\
+    "extra": {\
+        "installer-paths": {\
+            "Packages\/Extensions\/{$name}\/": ["type:aimeos-extension"]\
+        }\
+    }\
+}/' > composer.json.new
+mv composer.json.new composer.json
+
+composer require aimeos/aimeos-flow ~1.0
+rm -rf Packages/Application/Aimeos.Shop
+mv ../aimeos-flow Packages/Application/Aimeos.Shop
+composer dump-autoload
+
+mysql -e 'create database aimeos;'
+printf "TYPO3:\n  Flow:\n    persistence:\n      backendOptions:\n        dbname: 'aimeos'\n        user: 'root'" > Configuration/Testing/Settings.yaml
