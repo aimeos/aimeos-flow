caacb97f9da5cc6dfd528d4928046e13eed69860
Toco <toco@aimeos.org>
2016-04-18 14:11:23 +0200

    Fixed switching to disabled sites in admin interfaces

--- Classes/Aimeos/Shop/Controller/JqadmController.php
+++ Classes/Aimeos/Shop/Controller/JqadmController.php
@@ -181,7 +181,7 @@ class JqadmController extends \TYPO3\Flow\Mvc\Controller\ActionController
 		$lang = ( $this->request->hasArgument( 'lang' ) ? $this->request->getArgument( 'lang' ) : 'en' );
 		$templatePaths = $this->aimeos->get()->getCustomPaths( 'admin/jqadm/templates' );
 
-		$context = $this->context->get( $this->request );
+		$context = $this->context->get();
 		$context = $this->setLocale( $context, $sitecode, $lang );
 
 		$config = $context->getConfig();
