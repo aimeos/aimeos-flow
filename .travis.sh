922afac8635a905603650529d94a3211857a4f9d
Toco <toco@aimeos.org>
2016-12-27 15:38:23 +0100

    Use 2016.10.x-dev branch for testing

--- .travis.sh
+++ .travis.sh
@@ -13,7 +13,7 @@ cat composer.json | sed 's/^\}$/,\
 }/' > composer.json.new
 mv composer.json.new composer.json
 
-composer require aimeos/aimeos-flow dev-master
+composer require aimeos/aimeos-flow 2016.10.x-dev
 rm -rf Packages/Application/Aimeos.Shop
 mv ../aimeos-flow Packages/Application/Aimeos.Shop
 composer dump-autoload
