f45c0732bf157afa4f79794e96012e06e8a1cf9e
Toco <toco@aimeos.org>
2017-01-28 18:02:27 +0100

    Use coveralls 1.0 package

--- .travis.sh
+++ .travis.sh
@@ -5,12 +5,6 @@ set -ev
 cat composer.json | sed 's/^\}$/,\
     "minimum-stability": "dev",\
     "prefer-stable": true,\
-    "repositories": [\
-        {\
-            "type": "vcs",\
-            "url": "https:\/\/github.com\/aimeos\/php-coveralls.git"\
-        }\
-    ],\
     "extra": {\
         "installer-paths": {\
             "Packages\/Extensions\/{$name}\/": ["type:aimeos-extension"]\
@@ -19,7 +13,7 @@ cat composer.json | sed 's/^\}$/,\
 }/' > composer.json.new
 mv composer.json.new composer.json
 
-composer require --no-update aimeos/aimeos-flow:dev-master satooshi/php-coveralls:dev-master
+composer require --no-update aimeos/aimeos-flow:dev-master satooshi/php-coveralls:~1.0
 composer update
 rm -rf Packages/Application/Aimeos.Shop
 mv ../aimeos-flow Packages/Application/Aimeos.Shop
