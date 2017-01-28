dcc22c4d9a0ffbb3576dc54be163c2888fbb718b
Aimeos <aimeos@aimeos.org>
2017-01-28 19:26:57 +0100

    Added coverage with coveralls (#14)

--- .travis.sh
+++ .travis.sh
@@ -5,6 +5,12 @@ set -ev
 cat composer.json | sed 's/^\}$/,\
     "minimum-stability": "dev",\
     "prefer-stable": true,\
+    "repositories": [\
+        {\
+            "type": "vcs",\
+            "url": "https:\/\/github.com\/aimeos\/php-coveralls.git"\
+        }\
+    ],\
     "extra": {\
         "installer-paths": {\
             "Packages\/Extensions\/{$name}\/": ["type:aimeos-extension"]\
@@ -13,7 +19,8 @@ cat composer.json | sed 's/^\}$/,\
 }/' > composer.json.new
 mv composer.json.new composer.json
 
-composer require aimeos/aimeos-flow dev-master
+composer require --no-update aimeos/aimeos-flow:dev-master satooshi/php-coveralls:dev-master
+composer update
 rm -rf Packages/Application/Aimeos.Shop
 mv ../aimeos-flow Packages/Application/Aimeos.Shop
 composer dump-autoload
